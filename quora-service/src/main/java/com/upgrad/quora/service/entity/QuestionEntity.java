101e7880154fe71da40fb4be7cd8a6884e6891c0
pillella <44552477+pillella@users.noreply.github.com>
2019-02-10 00:50:10 -0600

    adding entity bean for relationship

--- quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
@@ -1,5 +1,9 @@
 package com.upgrad.quora.service.entity;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -11,6 +15,10 @@ import java.time.ZonedDateTime;
 
 @Entity
 @Table(name = "question", schema = "quora")
+@NamedQueries({
+        @NamedQuery(name = "questionByAccessToken", query = "select q from QuestionEntity q where q.user = :user "),
+        @NamedQuery(name = "questionByUuid", query = "select q from QuestionEntity q where q.uuid = :uuid ")
+})
 public class QuestionEntity implements Serializable {
 
     @Id
@@ -35,7 +43,7 @@ public class QuestionEntity implements Serializable {
     @ManyToOne
     @JoinColumn(name = "user_id")
     @OnDelete(action = OnDeleteAction.CASCADE)
-    private Integer user_id;
+    private UserEntity user;
 
     public Integer getQuestion_id() {
         return question_id;
@@ -69,11 +77,26 @@ public class QuestionEntity implements Serializable {
         this.date = date;
     }
 
-    public Integer getUser_id() {
-        return user_id;
+    public UserEntity getUser() {
+        return user;
     }
 
-    public void setUser_id(Integer user_id) {
-        this.user_id = user_id;
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return new EqualsBuilder().append(this, obj).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
     }
 }
